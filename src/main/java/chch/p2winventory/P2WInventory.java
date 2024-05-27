c38311e1436336f3d999de46cc8f844451aa26bb
twentysdev <fedenev.ilya.dm@gmail.com>
2024-05-27 18:49:27 +0700

    Initial commit

---
+++ src/main/java/chch/p2winventory/P2WInventory.java
@@ -0,0 +1,17 @@
+package chch.p2winventory;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class P2WInventory extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
