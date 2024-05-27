e6289d2ebf12e61cef37b9ef6b7803c99e59da32
twentysdev <fedenev.ilya.dm@gmail.com>
2024-05-28 00:12:35 +0700

    fix /getplayer

--- src/main/java/chch/p2winventory/EventListener.kt
+++ src/main/java/chch/p2winventory/EventListener.kt
@@ -1,27 +1,15 @@
 package chch.p2winventory
 
-import org.bukkit.Bukkit
-import org.bukkit.Material
-import org.bukkit.NamespacedKey
-import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
-import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.entity.EntityDamageEvent
-import org.bukkit.event.entity.EntityPickupItemEvent
-import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.inventory.InventoryClickEvent
-import org.bukkit.event.inventory.InventoryInteractEvent
-import org.bukkit.event.inventory.InventoryMoveItemEvent
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerInteractEvent
-import org.bukkit.event.player.PlayerPickupItemEvent
 import org.bukkit.event.player.PlayerRespawnEvent
 import org.bukkit.inventory.ItemStack
-import org.bukkit.inventory.PlayerInventory
-import org.bukkit.persistence.PersistentDataType
+
 
 class EventListener : Listener {
     @EventHandler
