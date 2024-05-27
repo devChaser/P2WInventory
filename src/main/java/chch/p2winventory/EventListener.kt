60d1dbaeb7ac5176eec83c04faeab7898f6f450b
Artyom <84637383+MZhuvka@users.noreply.github.com>
2024-05-27 20:03:04 +0300

    comments & importcleans

--- src/main/java/chch/p2winventory/EventListener.kt
+++ src/main/java/chch/p2winventory/EventListener.kt
@@ -1,29 +1,17 @@
 package chch.p2winventory
 
-import org.bukkit.Bukkit
-import org.bukkit.Material
-import org.bukkit.NamespacedKey
-import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
-import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.entity.EntityDamageEvent
-import org.bukkit.event.entity.EntityPickupItemEvent
-import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.inventory.InventoryClickEvent
-import org.bukkit.event.inventory.InventoryInteractEvent
-import org.bukkit.event.inventory.InventoryMoveItemEvent
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerInteractEvent
-import org.bukkit.event.player.PlayerPickupItemEvent
 import org.bukkit.event.player.PlayerRespawnEvent
 import org.bukkit.inventory.ItemStack
-import org.bukkit.inventory.PlayerInventory
-import org.bukkit.persistence.PersistentDataType
 
 class EventListener : Listener {
+    // Block unavailable slots when player respawns
     @EventHandler
     fun onPlayerRespawn(event: PlayerRespawnEvent) {
         val databaseManager = P2WInventory.instance!!.databaseManager
@@ -35,26 +23,28 @@ class EventListener : Listener {
             }
         }
     }
+    // Don't drop slotblockers®️
     @EventHandler
     fun onPlayerDropItem(event: PlayerDropItemEvent) {
         if (P2WInventory.instance!!.itemIsUnavailable(event.itemDrop.itemStack)) {
             event.isCancelled = true
         }
     }
+    // Can't drop/move/remove slotblockers®️ from your inventory
     @EventHandler
     fun onInventoryClick(event: InventoryClickEvent) {
         if (P2WInventory.instance!!.itemIsUnavailable(event.currentItem!!)) {
             event.isCancelled = true
         }
     }
-
+    // Can't place slotblockers®️ from your inventory
     @EventHandler
     fun onPlayerInteract(event: PlayerInteractEvent) {
         if (P2WInventory.instance!!.itemIsUnavailable(event.item!!)) {
             event.isCancelled = true
         }
     }
-
+    // Don't drop slotblockers®️ on player's death
     @EventHandler
     fun onEntityDamage(event: EntityDamageEvent) {
         if (event.entity !is Player) return
